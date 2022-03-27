c5462a33c561ed6060b1cf6437460e05153048e8
Blaine Simpson <blaine.simpson@admc.com>
2022-03-27 17:31:48 -0400

    Major design advance of MethodCaller

--- src/main/groovy/com/admc/jsonproxy/MethodCaller.groovy
+++ src/main/groovy/com/admc/jsonproxy/MethodCaller.groovy
@@ -3,6 +3,7 @@ package com.admc.jsonproxy
 import java.util.logging.Level
 import java.lang.reflect.Executable
 import java.lang.reflect.Parameter
+import java.lang.reflect.Constructor
 import com.admc.groovy.GroovyUtil
 
 @groovy.util.logging.Log(value = 'logger')
@@ -10,7 +11,7 @@ class MethodCaller {
     private Class cl
     private Object inst
     private String methodName
-    private List<Object> paramVals
+    private Object[] paramVals
     private Executable executable
 
     private MethodCaller(final Class cl, final Object inst,
@@ -18,8 +19,8 @@ class MethodCaller {
         this.cl = cl
         this.inst = inst
         this.methodName = methodName
-        paramVals = params
-        final int paramCount = paramVals.size()
+        paramVals = params.toArray()
+        final int paramCount = paramVals.length
         List<Class> pTypes = params.collect() { it.getClass() }
         def initialCandidates = methodName == null ? cl.declaredConstructors : cl.declaredMethods
         if (initialCandidates.size() < 1)
@@ -46,13 +47,23 @@ class MethodCaller {
     }
 
     /**
+     * Attempts .executable executions with variants of paramVals until one succees.
+     * Throws if non succeeds.
+     */
+    def exec() {
+        executable instanceof Constructor ?
+            executable.newInstance(paramVals) :
+            executable.invoke(inst, paramVals)
+    }
+
+    /**
      * Constructor invocation
      */
     static def call(final Class cl, final List<Object> params) {
         logger.log Level.INFO, "Constructor call for {0}-param {1}", params.size(), cl.simpleName
         // Don't yet know if have the same invoke-elicits-JVM-restart issue as the
         // instance method() call method below.
-        new MethodCaller(cl, null, null, params).executable.newInstance(params as Object[])
+        new MethodCaller(cl, null, null, params).exec()
     }
 
     /**
@@ -67,8 +78,7 @@ class MethodCaller {
         // has nothing to do with resolving inst or params here.
         //logger.log Level.WARNING, "Invoking {0}.{1} of {2}",
           //cs.keySet()[0].declaringClass.simpleName, methodName, inst.getClass().simpleName
-        new MethodCaller(inst.getClass(), inst, methodName, params).executable.
-          invoke(inst, params as Object[])
+        new MethodCaller(inst.getClass(), inst, methodName, params).exec()
     }
 
     /**
@@ -79,6 +89,6 @@ class MethodCaller {
           params.size(), cl.simpleName, methodName
         // Don't yet know if have the same invoke-elicits-JVM-restart issue as the
         // instance method() call method below.
-        new MethodCaller(cl, null, methodName, params).executable.invoke(null, params as Object[])
+        new MethodCaller(cl, null, methodName, params).exec()
     }
 }
